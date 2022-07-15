29705fb929980c8a507208211f56eb9051bf0641
shrir <shriraj1223shetty@gmail.com>
2022-07-15 16:32:43 +0530

    shriraj

--- clinic/settings.py
+++ clinic/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'clinic.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -94,7 +94,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
 }
-}
+}"""
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
